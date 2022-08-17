ffd47dd2c1742051ccdb14bb47d8565431d15876
Leonid Filippov <ski20173@yandex.ru>
2022-08-17 18:52:56 +0200

    Added flask server

--- runner.py
+++ runner.py
@@ -4,6 +4,7 @@ import logging
 from cogwatch import watch
 from discord.ext import commands
 from settings import SECRET_KEY
+from webserver import keep_alive
 
 logging.basicConfig(level=logging.INFO)
 
@@ -25,6 +26,7 @@ class Runner(commands.Bot):
 
 
 async def main():
+    keep_alive()
     client = Runner()
     await client.start(SECRET_KEY)
 
